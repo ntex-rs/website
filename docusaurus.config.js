fd2bd542b72f8ba58f2883c5b2b87197fdb002f1
Nikita Bishonen <git@bshn.rs>
2024-03-25 11:57:46 +0100

    Update docusaurus.config.js - provide correct edit URL (#6)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/ntex-rs/ntex-website/edit/master/',
+            'https://github.com/ntex-rs/website/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

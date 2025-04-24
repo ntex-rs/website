a6ca954c2576f078e587cbb01a036d77ef528341
nhjjgjjg <nhjjgjjg@163.com>
2025-04-24 23:06:23 +0800

    update: keep_alive.rs (#15)

--- examples/server/src/keep_alive.rs
+++ examples/server/src/keep_alive.rs
@@ -6,7 +6,7 @@ async fn main() -> std::io::Result<()> {
     let app_factory = web::App::new;
     // Set keep-alive to 75 seconds
     let _one =
-        web::HttpServer::new(app_factory).keep_alive(ntex::time::Seconds(5));
+        web::HttpServer::new(app_factory).keep_alive(ntex::time::Seconds(75));
 
     // Use OS's keep-alive (usually quite long)
     let _two =
